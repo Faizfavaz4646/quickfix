6f9ee7d29cd0765d8348f634e9b3bfa0d69a5cff
Faiz Favaz <faizfavaz7@gmail.com>
2025-08-26 16:00:12 +0530

    created new request card and active jobs in worker dashbord field

--- src/components/worker/WorkerActiveJobs.tsx
+++ src/components/worker/WorkerActiveJobs.tsx
@@ -32,7 +32,7 @@ export default function ActiveJobs() {
         {activeJobCount}
       </p>
 
-      <p className="text-sm text-gray-500">Ongoing </p>
+      <p className="text-sm text-gray-500">Currently in progress</p>
     </div>
   );
 }
